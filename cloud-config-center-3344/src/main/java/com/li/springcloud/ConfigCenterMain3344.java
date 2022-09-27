b8a98e5f02daa11ee2794a023d53c1ff6554414a
lyhgithub86 <liyuhao8690@163.com>
2022-09-27 10:47:19 +0800

    1.0

---
+++ cloud-config-center-3344/src/main/java/com/li/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,24 @@
+package com.li.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+import javax.security.auth.login.Configuration;
+
+/**
+ * Description: new java files header..
+ *
+ * @author lixs
+ * @version 1.0
+ * @date 2022/9/27 10:44
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+        System.out.println("configCenter3344 start");
+    }
+}
